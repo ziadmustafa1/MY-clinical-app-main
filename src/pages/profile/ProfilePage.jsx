a9074dd1569203a4d1a2b7cc926fdda2c3b386ae
ziadmustafa1 <123ziadmstfy@gmail.com>
2023-12-18 02:16:36 +0200

    z

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Profile from '../../components/profile/profile'
+import Profile from '../../components/profile/Profile'
 import NavRouter from '../../components/utily/NavRouter'
 
 const ProfilePage = () => {
